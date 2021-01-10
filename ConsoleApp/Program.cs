edd4d3e5b8e8045e1843034fa348997c9af5598c
Thiago Almeida <tbalmeida@gmail.com>
2021-01-09 18:18:10 -0700

    Add console app for step 6

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static class QuestionClass
+        {
+            public static List<string> NamesList = new List<string>()
+            {
+                "Jimmy",
+                "Jeffrey",
+                "John",
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vog Code Challenge - Step 5\n");
+
+            byte counter = 0;
+
+            while (counter < QuestionClass.NamesList.Count)
+            {
+                Console.WriteLine($"Item {counter + 1} - { QuestionClass.NamesList[counter]}");
+                counter++;
+            }
+        }
+    }
+}
